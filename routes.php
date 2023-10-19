1531c3ff4a464fcffb6cea43a3ee16f3119177ff
Tunna Duong <tunnaduong@gmail.com>
2023-10-19 14:58:31 +0700

    initial commit

---
+++ routes.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__.'/router.php';
+
+// ##################################################
+// ##################################################
+// ##################################################
+
+// Static GET
+// In the URL -> http://localhost
+// The output -> Index
+get('/', 'views/index.php');
+
+// Dynamic GET. Example with 1 variable
+// The $id will be available in user.php
+get('/$id', 'views/get_link');
+
+
+// ##################################################
+// ##################################################
+// ##################################################
+// any can be used for GETs or POSTs
+
+// For GET or POST
+// The 404.php which is inside the views folder will be called
+// The 404.php has access to $_GET and $_POST
+any('/404','views/404.php');
---
+++ views/index.php
@@ -0,0 +1,23 @@
+<?php
+// To call this page, in the browser type:
+// http://localhost/
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tunna Duong Link Shortener</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="shortcut icon" href="/assets/images/tunnaduong.png" type="image/x-png">
+    <meta name="theme-color" content="#01C483" />
+</head>
+<body>
+    <center>
+        <h1>Link Shortener</h1>
+        <div class="btn btn-primary">Bấm vào đây để tiếp tục!</div>
+        <div class="btn"></div>
+    </center>
+</body>
+</html>
