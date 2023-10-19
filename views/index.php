907d6f140c5ce5dd1d9939d83f4ec53738b3832c
Tunna Duong <tunnaduong@gmail.com>
2023-10-19 16:29:15 +0700

    changes...

--- views/index.php
+++ views/index.php
@@ -12,12 +12,20 @@
     <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="shortcut icon" href="/assets/images/tunnaduong.png" type="image/x-png">
     <meta name="theme-color" content="#01C483" />
+    <script src="https://kit.fontawesome.com/be3d8625b2.js" crossorigin="anonymous"></script>
 </head>
-<body>
+<body onclick="">
     <center>
         <h1>Link Shortener</h1>
-        <div class="btn btn-primary">Bấm vào đây để tiếp tục!</div>
-        <div class="btn">Mở trong điện thoại (QR Code)</div>
+        <div onclick="" class="btn btn-primary">Bấm vào đây để tiếp tục!</div>
+        <div onclick="" class="btn">Mở trong điện thoại (QR Code)</div>
+        <div onclick="" class="btn">Xem thông tin chi tiết link</div>
+        <h2><span><i class="fas fa-share"></i> Chia sẻ</span></h2>
+        <div class="social">
+            <i onclick="" class="fab fa-facebook"></i>
+            <i onclick="" class="fab fa-twitter-square"></i>
+            <i onclick="" class="fas fa-copy"></i>
+        </div>
     </center>
 </body>
 </html>
