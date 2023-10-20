038e59094a9c86542dd38c5dc311eb1c73d96054
Tunna Duong <tunnaduong@gmail.com>
2023-10-20 18:19:35 +0700

    kp

--- views/404.php
+++ views/404.php
@@ -1,5 +1,44 @@
-<?php
-// To call this page, in the browser type a route that doesn't exist like:
-// http://localhost/test/route
+<!DOCTYPE html>
+<html lang="en">
 
-echo 'PAGE NOT FOUND';
+<head>
+    <?php
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/head.php';
+    // Get the current website URL
+    $currentUrl = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    ?>
+    <title><?php echo isset($row['link_title']) ? $row['link_title'] : "Tunna Duong Link Shortener" ?></title>
+    <meta property="og:title" content="<?php echo $row['link_title'] ?>" />
+    <meta property="og:description" content="<?php echo isset($row['link_excerpt']) ? $row['link_excerpt'] : "Công cụ rút gọn link được tạo bởi Tunna Duong" ?>" />
+    <meta property="og:type" content="website.url-shortener" />
+    <meta property="og:url" content="<?php echo $currentUrl ?>" />
+    <meta property="og:image" content="<?php echo isset($row['link_preview_url']) ? $row['link_preview_url'] : "/assets/images/link.jpg" ?>" />
+</head>
+
+<body onclick="">
+    <center onclick>
+        <h1>Link Shortener</h1>
+        <img src="/assets/images/404.png" class="_404" alt="404 Not Found">
+        <div class="ads">
+            <div id="awn-z7610822"></div>
+        </div>
+        <div>
+            <script data-cfasync="false" type="text/javascript" src="//brightonclick.com/a/display.php?r=7610902"></script>
+        </div>
+        <footer>
+            <p class="footer--copyright">
+                <span id="footer--mobile">© 2023 Duong Tung Anh<br /><span style="color: white; font-weight: 300; font-size: 15px">All rights reserved</span></span>
+                <span id="footer--desktop">© 2023 Duong Tung Anh. All rights reserved.</span>
+            </p>
+            <p class="footer--fun">
+                Được làm bằng 💕 <i>tình yêu</i>, 🔥 <i>nhiệt huyết</i>, ⌨️
+                <i>bàn phím</i> và rất nhiều ☕️ <i>cà phê</i>.
+            </p>
+        </footer>
+    </center>
+    <?php
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php';
+    ?>
+</body>
+
+</html>
--- views/index.php
+++ views/index.php
@@ -1,71 +1,28 @@
-<?php
-// To call this page, in the browser type:
-// http://localhost/
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <?php
     require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/head.php';
+    // Get the current website URL
+    $currentUrl = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     ?>
+    <title><?php echo isset($row['link_title']) ? $row['link_title'] : "Tunna Duong Link Shortener" ?></title>
+    <meta property="og:title" content="<?php echo $row['link_title'] ?>" />
+    <meta property="og:description" content="<?php echo isset($row['link_excerpt']) ? $row['link_excerpt'] : "Công cụ rút gọn link được tạo bởi Tunna Duong" ?>" />
+    <meta property="og:type" content="website.url-shortener" />
+    <meta property="og:url" content="<?php echo $currentUrl ?>" />
+    <meta property="og:image" content="<?php echo isset($row['link_preview_url']) ? $row['link_preview_url'] : "/assets/images/link.jpg" ?>" />
 </head>
 
 <body onclick="">
     <center onclick>
         <h1>Link Shortener</h1>
-        <div id="next_btn" onclick="alert('Chức năng đang hoàn thiện!')" class="btn btn-primary disabled-button">Vui lòng đợi 6 giây...</div>
-        <div id="openModalBtn" onclick="modal.show()" class="btn">Mở trong điện thoại (QR Code)</div>
-        <div id="myModal" class="modal">
-            <div class="modal-content">
-                <span class="close">&times;</span>
-                <p>Quét mã QR này bằng điện thoại của bạn</p>
-                <div class="line"></div>
-                <div id="qrcode"></div>
-            </div>
-        </div>
-        <script type="text/javascript">
-            new QRCode(document.getElementById("qrcode"), window.location.href);
-        </script>
-        <a href="#link_info">
-            <div onclick class="btn">Xem thông tin chi tiết link</div>
-        </a>
-        <h2><span><i class="fas fa-share"></i> Chia sẻ</span></h2>
-        <div class="social">
-            <i onclick="alert('Chức năng đang hoàn thiện!')" class="fab fa-facebook"></i>
-            <i onclick="alert('Chức năng đang hoàn thiện!')" class="fab fa-twitter-square"></i>
-            <i onclick="alert('Chức năng đang hoàn thiện!')" class="fas fa-copy"></i>
-        </div>
+        <img src="/assets/images/dude.jpg" class="_404 dude" alt="404 Not Found">
+        <h2>Quay lại trang trước đi. Ở đây không có gì đâu ^^</h2>
         <div class="ads">
             <div id="awn-z7610822"></div>
         </div>
-        <h3 id="link_info">Thông tin liên kết</h3>
-        <table class="table">
-            <tbody>
-                <tr>
-                    <td>ID</td>
-                    <td>gido</td>
-                </tr>
-                <tr>
-                    <td>Mật khẩu</td>
-                    <td>Không có</td>
-                </tr>
-                <tr>
-                    <td>Lượt truy cập</td>
-                    <td>43532</td>
-                </tr>
-                <tr>
-                    <td>Thời gian tạo</td>
-                    <td>9 tháng trước</td>
-                </tr>
-            </tbody>
-        </table>
-        <p class="tag">Thẻ:
-            <span class="badge">gì đó</span>
-            <span class="badge">test</span>
-            <span class="badge">thử nghiệm</span>
-        </p>
         <div>
             <script data-cfasync="false" type="text/javascript" src="//brightonclick.com/a/display.php?r=7610902"></script>
         </div>
