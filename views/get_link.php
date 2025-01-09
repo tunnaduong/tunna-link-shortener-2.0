98080c1b8ed93c92518d9aab9711a280c0e09567
Dương Tùng Anh <tunnaduong@gmail.com>
2025-01-10 02:40:56 +0700

    Refactor 404 error page layout and integrate ad scripts for improved functionality

--- views/get_link.php
+++ views/get_link.php
@@ -216,7 +216,21 @@ if ($result->num_rows > 0) {
     <body onclick="">
         <center onclick>
             <h1>Link Shortener</h1>
-            <img src="/assets/images/404.png" class="_404 dude" alt="404 Not Found">
+            <div>
+                <img src="/assets/images/404.png" class="_404 dude" alt="404 Not Found">
+            </div>
+            <script type="text/javascript">
+                atOptions = {
+                    'key': '2af190ba44f51f05b0f68a0224e3d5fc',
+                    'format': 'iframe',
+                    'height': 250,
+                    'width': 300,
+                    'params': {}
+                };
+            </script>
+            <script type="text/javascript" src="//www.highperformanceformat.com/2af190ba44f51f05b0f68a0224e3d5fc/invoke.js"></script>
+            <script async="async" data-cfasync="false" src="//pl25523691.profitablecpmrate.com/e19b2044d36d5ec26b29ac25e2e560a9/invoke.js"></script>
+            <div id="container-e19b2044d36d5ec26b29ac25e2e560a9" style="color: white; max-width: 600px"></div>
             <footer>
                 <p class="footer--copyright">
                     <span id="footer--mobile">© 2023 Duong Tung Anh<br /><span style="color: white; font-weight: 300; font-size: 15px">All rights reserved</span></span>
