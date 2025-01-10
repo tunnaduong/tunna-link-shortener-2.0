a4935122cc88582b1e4242765e137d9f21fb57e8
Duong Tung Anh <tunnaduong@gmail.com>
2025-01-10 13:42:58 +0700

    Add detailed link information button and improve script loading in get_link.php

--- views/get_link.php
+++ views/get_link.php
@@ -139,6 +139,11 @@ if ($result->num_rows > 0) {
                         </form>
                         <?php
                         echo renderQRCodeSection();
+                        ?>
+                        <a href="#link_info" class="scroll-link">
+                            <div class="btn">Xem thông tin chi tiết link</div>
+                        </a>
+                        <?php
                         echo renderShareOptions();
                         echo renderAds($row);
                         $sql = "SELECT count(*) as total FROM tracker WHERE ref_code = '$id'";
@@ -205,6 +210,11 @@ if ($result->num_rows > 0) {
                     </form>
                     <?php
                     echo renderQRCodeSection();
+                    ?>
+                    <a href="#link_info" class="scroll-link">
+                        <div class="btn">Xem thông tin chi tiết link</div>
+                    </a>
+                    <?php
                     echo renderShareOptions();
                     echo renderAds($row);
                     $sql = "SELECT count(*) as total FROM tracker WHERE ref_code = '$id'";
@@ -227,7 +237,7 @@ if ($result->num_rows > 0) {
                             'params': {}
                         };
                     </script>
-                    <script type="text/javascript" src="//www.highperformanceformat.com/2af190ba44f51f05b0f68a0224e3d5fc/invoke.js"></script>
+                    <script async="async" data-cfasync="false" type="text/javascript" src="//www.highperformanceformat.com/2af190ba44f51f05b0f68a0224e3d5fc/invoke.js"></script>
                     <?php
                     echo renderTags($row['tag']);
                     ?>
@@ -299,76 +309,77 @@ if ($result->num_rows > 0) {
                 <?php
                 echo renderFooter();
                 ?>
-                <script>
-                    var width = window.screen.width;
-                    var height = window.screen.height;
-                    const referrer = document.referrer;
-                    const data = {
-                        id: "<?php echo $row['code'] ?>",
-                        size: width + 'x' + height,
-                        ref: referrer
-                    };
-                    $.ajax({
-                        type: "POST",
-                        url: "/api/tracker",
-                        data: data,
-                        success: function(response) {
-                            console.log(response);
-                        }
-                    });
-                    if (/^\?fbclid=/.test(location.search)) {
-                        location.replace(location.href.replace(/\?fbclid.+/, ""));
-                    }
-                </script>
-                <script src="/assets/js/script.js"></script>
-            <?php
+        </center>
+        <script>
+            var width = window.screen.width;
+            var height = window.screen.height;
+            const referrer = document.referrer;
+            const data = {
+                id: "<?php echo $row['code'] ?>",
+                size: width + 'x' + height,
+                ref: referrer
+            };
+            $.ajax({
+                type: "POST",
+                url: "/api/tracker",
+                data: data,
+                success: function(response) {
+                    console.log(response);
+                }
+            });
+            if (/^\?fbclid=/.test(location.search)) {
+                location.replace(location.href.replace(/\?fbclid.+/, ""));
+            }
+        </script>
+        <script src="/assets/js/script.js"></script>
+    <?php
             }
         } else {
-            ?>
-            <!DOCTYPE html>
-            <html lang="en">
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-            <head>
-                <?php
-                require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/head.php';
-                $currentUrl = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-                ?>
-                <title><?php echo isset($row['link_title']) ? $row['link_title'] : "Tunna Duong Link Shortener" ?></title>
-                <meta property="og:title" content="<?php echo $row['link_title'] ?>" />
-                <meta property="og:description" content="<?php echo isset($row['link_excerpt']) ? $row['link_excerpt'] : "Công cụ rút gọn link được tạo bởi Tunna Duong" ?>" />
-                <meta property="og:type" content="website.url-shortener" />
-                <meta property="og:url" content="<?php echo $currentUrl ?>" />
-                <meta property="og:image" content="<?php echo isset($row['link_preview_url']) ? $row['link_preview_url'] : "/assets/images/link.jpg" ?>" />
-            </head>
-
-            <body>
-                <center>
-                    <h1>Link Shortener</h1>
-                    <div>
-                        <img src="/assets/images/404.png" class="_404 dude" alt="404 Not Found">
-                    </div>
-                    <script type="text/javascript">
-                        atOptions = {
-                            'key': '2af190ba44f51f05b0f68a0224e3d5fc',
-                            'format': 'iframe',
-                            'height': 250,
-                            'width': 300,
-                            'params': {}
-                        };
-                    </script>
-                    <script type="text/javascript" src="//www.highperformanceformat.com/2af190ba44f51f05b0f68a0224e3d5fc/invoke.js"></script>
-                    <script async="async" data-cfasync="false" src="//pl25523691.profitablecpmrate.com/e19b2044d36d5ec26b29ac25e2e560a9/invoke.js"></script>
-                    <div id="container-e19b2044d36d5ec26b29ac25e2e560a9" style="color: white; max-width: 600px"></div>
-                    <?= renderFooter() ?>
-                </center>
-                <?php
-                require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php';
-                ?>
-            </body>
-
-            </html>
+    <head>
         <?php
+            require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/head.php';
+            $currentUrl = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        ?>
+        <title><?php echo isset($row['link_title']) ? $row['link_title'] : "Tunna Duong Link Shortener" ?></title>
+        <meta property="og:title" content="<?php echo $row['link_title'] ?>" />
+        <meta property="og:description" content="<?php echo isset($row['link_excerpt']) ? $row['link_excerpt'] : "Công cụ rút gọn link được tạo bởi Tunna Duong" ?>" />
+        <meta property="og:type" content="website.url-shortener" />
+        <meta property="og:url" content="<?php echo $currentUrl ?>" />
+        <meta property="og:image" content="<?php echo isset($row['link_preview_url']) ? $row['link_preview_url'] : "/assets/images/link.jpg" ?>" />
+    </head>
+
+    <body>
+        <center>
+            <h1>Link Shortener</h1>
+            <div>
+                <img src="/assets/images/404.png" class="_404 dude" alt="404 Not Found">
+            </div>
+            <script type="text/javascript">
+                atOptions = {
+                    'key': '2af190ba44f51f05b0f68a0224e3d5fc',
+                    'format': 'iframe',
+                    'height': 250,
+                    'width': 300,
+                    'params': {}
+                };
+            </script>
+            <script type="text/javascript" src="//www.highperformanceformat.com/2af190ba44f51f05b0f68a0224e3d5fc/invoke.js"></script>
+            <script async="async" data-cfasync="false" src="//pl25523691.profitablecpmrate.com/e19b2044d36d5ec26b29ac25e2e560a9/invoke.js"></script>
+            <div id="container-e19b2044d36d5ec26b29ac25e2e560a9" style="color: white; max-width: 600px"></div>
+            <?= renderFooter() ?>
+        </center>
+        <?php
+            require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php';
+        ?>
+    </body>
+
+    </html>
+<?php
         }
         $conn->close();
         ob_end_flush(); // End output buffering and flush output
-        ?>
+?>
