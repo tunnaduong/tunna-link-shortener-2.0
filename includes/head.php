ded72b9e7672b9f7e23aa53fba37ac62a3864922
Duong Tung Anh <tunnaduong@gmail.com>
2025-01-11 10:37:02 +0700

    Add async loading for Google Ads script in head.php for improved performance

--- includes/head.php
+++ includes/head.php
@@ -11,3 +11,5 @@
     async defer>
 </script>
 <script src="/assets/js/script.js"></script>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3425905751761094"
+     crossorigin="anonymous"></script>
